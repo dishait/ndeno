eab551b956f1612b244684aed10ddaa659645270
markthree <1801982702@qq.com>
2023-03-19 14:12:51 +0800

    fix: Prevent duplicate install

--- src/pm.ts
+++ src/pm.ts
@@ -11,24 +11,26 @@ export function usePackageManager() {
     // Check if the value of ref exists
     if (ref.value) return;
     // Set the value of ref based on the existence of certain files
-    ref.value = await exist("pnpm-lock.yaml") ? "pnpm" : await exist("yarn.lock") ? "yarn" : "npm";
+    ref.value = await exist("pnpm-lock.yaml")
+      ? "pnpm"
+      : await exist("yarn.lock")
+      ? "yarn"
+      : "npm";
   }
-  
+
   function getCommand() {
-    const pm = ref.value
+    const pm = ref.value;
     const [command, ...args] = Deno.args;
-    
+
     // If there are no arguments, install dependencies by default
-    if (args.length === 0) return [pm, "install"];
+    if (Deno.args.length === 0) return [pm, "install"];
 
     // If the command is "i" and the package manager is yarn, convert it to "add"
     if (pm === "yarn" && command === "i") return [pm, "add", ...args];
 
-    // If the package manager is not npm, return the arguments directly
-    if (pm !== "npm") return [pm, ...Deno.args];
-
     // If the command is a script, convert it to "run" command
-    const isRunScript = !/^(run|install|test|publish|uninstall|help|add|remove|i)$/.test(command);
+    const isRunScript =
+      !/^(run|install|test|publish|uninstall|help|add|remove|i)$/.test(command);
 
     return isRunScript ? [pm, "run", ...Deno.args] : [pm, ...Deno.args];
   }
