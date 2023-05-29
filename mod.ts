7b2b70d3e535ff10d5bb7153b946b6c0b13843fd
markthree <1801982702@qq.com>
2023-05-29 14:10:34 +0800

    chore: update version

--- mod.ts
+++ mod.ts
@@ -38,7 +38,7 @@ if (import.meta.main) {
 
   const commander = new Command()
     .name("n")
-    .version("1.1.1")
+    .version("1.1.2")
     .description(`Command line tool created by deno to manage node projects`)
     .action(() => execaInstall(pm))
 
