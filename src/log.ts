61a67f1bc3b76d316127532f88a879242789fa6d
markthree <1801982702@qq.com>
2023-09-11 13:26:45 +0800

    fix(logClean): slash dir

--- src/log.ts
+++ src/log.ts
@@ -1,7 +1,8 @@
 import { brightGreen, gray } from "https://deno.land/std@0.201.0/fmt/colors.ts"
+import { slash } from "https://deno.land/x/easy_std@v0.5.0/src/path.ts"
 
 export function logClean(dir: string) {
   console.log(
-    `${brightGreen("√ clean")} ${gray(dir)}`,
+    `${brightGreen("√ clean")} ${gray(slash(dir))}`,
   )
 }
