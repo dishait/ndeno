747adc47c828687d0e48eb1ae609fd15585ac0e9
markthree <1801982702@qq.com>
2024-01-07 17:59:34 +0800

    feat: add force option

--- src/cli.ts
+++ src/cli.ts
@@ -70,6 +70,7 @@ export async function action(pm: PM) {
         brightYellow("(only pnpm)")
       }`,
     )
+    .option("-f, --force", "force install deps")
     .arguments("[...deps:string]")
     .action(
       (options, ...deps) => _install(pm, deps, formatOptions(options)),
